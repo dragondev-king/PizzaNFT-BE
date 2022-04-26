04151d6d22529118215e8f3790e53a6ee6628e03
Dragondev-King <dragondev1017@gmail.com>
2022-04-27 01:07:19 +0900

    create nftItem & Owner models

--- app/models/model.js
+++ app/models/model.js
@@ -86,5 +86,35 @@ module.exports = mongoose => {
 
   const Follow = mongoose.model("follow", follow)
 
-  return { Profile, Auction, Bid, History,  Follow};
+  var nftItem = mongoose.Schema(
+    {
+      id: Number
+    },
+    { timestamps: true }
+  )
+  nftItem.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const NftItem = mongoose.model("nftItem", nftItem)
+
+  var owner = mongoose.Schema(
+    {
+      tokenAddress: String,
+      tokenId: Number,
+      ownerOf: String
+    },
+    {timestamps: true}
+  )
+  owner.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Owner = mongoose.model("owner", owner)
+
+  return { Profile, Auction, Bid, History,  Follow, NftItem, Owner };
 };
